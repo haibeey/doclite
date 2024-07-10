cd084da6aea74db7f68f17b778a5a4ff936609a6
Abraham Akerele <aakerele@seismic.com>
2024-07-10 13:03:48 +0100

    clean up

--- examples/golang/example.go
+++ examples/golang/example.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"github.com/haibeey/doclite"
 )
-func main(){
+
+func main() {
 	type Employer struct {
 		Name    string
 		Address string
@@ -18,12 +19,11 @@ func main(){
 	for i := 0; i < 20; i++ {
 		baseCollection.Insert(
 			&Employer{
-				Name: fmt.Sprintf("%d docklite", i),
+				Name:    fmt.Sprintf("%d docklite", i),
 				Address: "20, daniel's street, Abuja",
 			},
 		)
 	}
-	
 
 	e := &Employer{}
 	baseCollection.FindOne(14, e)
@@ -32,66 +32,61 @@ func main(){
 	e = &Employer{}
 	baseCollection.FindOne(16, e)
 	fmt.Println(e)
-	
+
 	//find all document matching address==doe
-	cur:=baseCollection.Find(
-		&Employer{Address:"20, daniel's street, Abuja"},
-		&Employer{},
+	cur := baseCollection.Find(
+		&Employer{Address: "20, daniel's street, Abuja"},
 	)
-	count:=0
+	count := 0
 	for {
-		emp:=cur.Next()
-		if emp==nil{
+		emp := cur.Next()
+		if emp == nil {
 			break
 		}
 		fmt.Println(emp)
 		count++
 	}
-	fmt.Println("Found ",count, "documents")
-
+	fmt.Println("Found ", count, "documents")
 
 	//sub collection
-	type User struct{
-		Name string
+	type User struct {
+		Name    string
 		Address string
 		Friends []User
 	}
 
-	//create a new collection from 
+	//create a new collection from
 	userCollection := baseCollection.Collection("user")
 	//Insert 20 new document
 	for i := 0; i < 20; i++ {
 		userCollection.Insert(
 			&User{
-				Name: fmt.Sprintf("%d docklite %s", i,"user"),
+				Name:    fmt.Sprintf("%d docklite %s", i, "user"),
 				Address: "20, daniel's street, Abuja",
-				Friends:[]User{
-					User{Name:fmt.Sprintf("%d testing %s", i,"user")},
+				Friends: []User{
+					{Name: fmt.Sprintf("%d testing %s", i, "user")},
 				},
 			},
 		)
 	}
-	
+
 	u := &User{}
 	userCollection.FindOne(14, u)
-	fmt.Println(u)
 	userCollection.DeleteOne(16)
 	u = &User{}
 	userCollection.FindOne(16, u)
-	fmt.Println(u)
-	u = &User{}
-	doe:=&User{Address:"20, daniel's street, Abuja"}
-	cur=userCollection.Find(doe,u)
-	count=0
+	doe := &User{Address: "20, daniel's street, Abuja"}
+	cur = userCollection.Find(doe)
+	count = 0
 	for {
-		emp:=cur.Next()
-		if emp==nil{
+		a := cur.NextObject(u)
+		if a == nil {
 			break
 		}
 		count++
-		fmt.Println(u.Name)
+		fmt.Println(a)
 	}
-	fmt.Println("Found ",count, "documents")
+	fmt.Println("Found ", count, "documents")
 
 	db.Close()
 }
